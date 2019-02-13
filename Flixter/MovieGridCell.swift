ab188fd976ba40f9616a31f027383c92c5248c79
Hadia Andar <hadiaandar@Hadias-MacBook-Air.local>
2019-02-12 21:05:11 -0800

    flix part 2

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCellCollectionViewCell.swift
+//  Flixter
+//
+//  Created by Hadia Andar on 2/12/19.
+//  Copyright © 2019 Hadia Andar. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
